2fd3de411facbddee6a0875ff93eaa21cb36c88b
Ashwin <raiashwin005@gmail.com>
2026-06-29 11:11:06 +0530

    Add LSP for prisma files.

--- init.lua
+++ init.lua
@@ -81,13 +81,74 @@ require("lazy").setup({
   -- Terminal
   { "akinsho/toggleterm.nvim",         version = "*",      config = true },
 
+  -- GitHub Copilot
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+
   -- QoL
   "windwp/nvim-autopairs",
   "tpope/vim-commentary",
 
   -- UI Enhancements
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  -- TODO Comments: highlights and searches TODO/FIX/HACK across languages
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup({
+        signs = true,
+        keywords = {
+          FIX = { icon = " ", color = "#ff6b6b" },
+          TODO = { icon = " ", color = "#ff6b6b" },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning" },
+          NOTE = { icon = " ", color = "hint" },
+        },
+        highlight = { multiline = false, before = "", after = "fg" },
+      })
+
+      vim.keymap.set("n", "<leader>tt", "<cmd>TodoTelescope<CR>", { silent = true, desc = "Todos (Telescope)" })
+      vim.keymap.set("n", "<leader>tq", "<cmd>TodoQuickFix<CR>", { silent = true, desc = "Todos (QuickFix)" })
+    end,
+  },
   "nvim-treesitter/nvim-treesitter-context",
+
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add    = { text = "▎" },
+          change = { text = "▎" },
+          delete = { text = "▁" },
+        },
+      })
+    end,
+  },
+  {
+    "j-hui/fidget.nvim",
+    tag = "legacy",
+    config = function()
+      require("fidget").setup({ text = { spinner = "dots" } })
+    end,
+  },
 })
 
 -- ===========================
@@ -123,17 +184,30 @@ require("nvim-autopairs").setup({})
 require("lualine").setup({
   options = {
     theme = "rose-pine",
-    component_separators = "",
-    section_separators = "",
+    component_separators = { left = "", right = "" },
+    section_separators   = { left = "", right = "" },
     globalstatus = true,
   },
   sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { { "filename", path = 1 } }, -- Relative path
-    lualine_x = { "location" },
+    lualine_a = { "mode" },
+    lualine_b = {
+      { "branch", icon = "" },
+      {
+        "diff",
+        symbols = { added = " ", modified = " ", removed = " " },
+        source = function()
+          local gs = vim.b.gitsigns_status_dict
+          if gs then return { added = gs.added, modified = gs.changed, removed = gs.removed } end
+        end,
+      },
+    },
+    lualine_c = { { "filename", path = 1, symbols = { modified = " ●", readonly = " ", unnamed = "[No Name]" } } },
+    lualine_x = {
+      { "diagnostics", sources = { "nvim_lsp" }, symbols = { error = " ", warn = " ", hint = " ", info = " " } },
+      "filetype",
+    },
     lualine_y = {},
-    lualine_z = {},
+    lualine_z = { "location" },
   },
 })
 
@@ -154,9 +228,22 @@ require("ibl").setup({
 -- ===========================
 require("nvim-tree").setup({
   view = { width = 35 },
+  git = { enable = true, ignore = false },
   renderer = {
     highlight_git = true,
-    icons = { show = { file = true, folder = true } },
+    icons = {
+      show = { git = true, file = true, folder = true },
+      glyphs = {
+        git = {
+          unstaged  = "✚",
+          staged    = "✔",
+          untracked = "★",
+          deleted   = "✖",
+          renamed   = "➜",
+          ignored   = "◌",
+        },
+      },
+    },
   },
   actions = { open_file = { quit_on_open = false } },
 })
@@ -165,6 +252,58 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 -- ===========================
 -- 🔍 Telescope
 -- ===========================
+
+-- Close NvimTree when opening a file (not a folder). If a directory
+-- is opened on startup, open the tree instead.
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = function(data)
+    local fname = data.file
+    if not fname or fname == "" then
+      -- no argument: do nothing
+      return
+    end
+    if vim.fn.isdirectory(fname) == 1 then
+      pcall(function()
+        require("nvim-tree.api").tree.open()
+      end)
+    else
+      -- ensure tree is closed when starting with a file
+      pcall(function()
+        require("nvim-tree.api").tree.close()
+      end)
+    end
+  end,
+})
+
+-- Close NvimTree when entering a regular file buffer, but don't close
+-- it when entering the NvimTree buffer itself. This lets you toggle the
+-- tree manually after opening a file.
+vim.api.nvim_create_autocmd({ "BufWinEnter", "BufReadPost" }, {
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+
+    -- don't act on the tree buffer itself
+    if ft == "NvimTree" or ft == "nvim-tree" then
+      return
+    end
+
+    local name = vim.api.nvim_buf_get_name(bufnr)
+    if name == "" then
+      return
+    end
+
+    -- only close if the buffer is not a directory and the tree is visible
+    if vim.fn.isdirectory(name) == 0 then
+      pcall(function()
+        local api = require("nvim-tree.api")
+        if api and api.tree and api.tree.is_visible and api.tree.is_visible() then
+          api.tree.close()
+        end
+      end)
+    end
+  end,
+})
 local actions = require("telescope.actions")
 require("telescope").setup({
   defaults = {
@@ -202,7 +341,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities(
 -- 🤖 LSP (Mason + lspconfig)
 -- ===========================
 -- Enable default servers
-local servers = { "gopls", "ts_ls", "yamlls", "tailwindcss", "jsonls", "clangd", "html", "cssls", "sqls" }
+local servers = { "gopls", "ts_ls", "yamlls", "tailwindcss", "jsonls", "clangd", "html", "cssls", "sqls", "prismals" }
 
 require("mason").setup()
 require("mason-lspconfig").setup({
@@ -246,9 +385,10 @@ cmp.setup({
     }),
   },
   sources = {
-    { name = "nvim_lsp" },
-    { name = "buffer" },
-    { name = "luasnip" },
+    { name = "copilot",  group_index = 2 },
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "luasnip",  group_index = 2 },
+    { name = "buffer",   group_index = 2 },
   },
 })
 
@@ -272,6 +412,14 @@ vim.diagnostic.config({
   virtual_text = true,
   float = { border = "rounded" },
   severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN]  = " ",
+      [vim.diagnostic.severity.HINT]  = "󰌵 ",
+      [vim.diagnostic.severity.INFO]  = " ",
+    },
+  },
 })
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
@@ -318,3 +466,17 @@ vim.keymap.set({ "n", "i", "v" }, "<A-w>", "<cmd>wincmd w<CR>")
 
 -- Toggle Side Directory (NvimTree)
 vim.keymap.set({ "n", "i", "v" }, "<C-b>", "<cmd>NvimTreeToggle<CR>")
+
+-- Toggle Copilot
+local copilot_enabled = true
+vim.keymap.set("n", "<leader>cp", function()
+  if copilot_enabled then
+    vim.cmd("Copilot disable")
+    copilot_enabled = false
+    vim.notify("Copilot disabled", vim.log.levels.INFO)
+  else
+    vim.cmd("Copilot enable")
+    copilot_enabled = true
+    vim.notify("Copilot enabled", vim.log.levels.INFO)
+  end
+end, { desc = "Toggle Copilot" })
